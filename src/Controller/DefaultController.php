858fa77294b9c257f1a7ef9e1c9908ba2a79a07d
anaasins <asinsanna@gmail.com>
2022-02-08 10:50:11 +0100

    Empezado diseño pagina mi perfil

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,37 +9,27 @@ use App\Repository\EjercicioRepository;
 
 class DefaultController extends AbstractController
 {
-  /**
-   * @Route("/", name="home_page")
-   */
+   #[Route('/', name: 'home_page')]
     public function homeAction(EjercicioRepository $ejercicioRepository): Response
     {
       //Capturar el repositorio de la tabla contra la bd
       $ejercicios = $ejercicioRepository->findThreeToShow();
       return $this->render('frontal/index.html.twig', array('ejercicios'=>$ejercicios));
     }
-    /**
-    * @Route("/login", name="login_page")
-     * login_page
-     */
+
+     #[Route('/login', name: 'login_page')]
     public function loginAction(): Response
     {
         return $this->render('frontal/login.html.twig');
     }
 
-    /**
-    * @Route("/contact", name="contact_page")
-     * contact_page
-     */
+     #[Route('/contact', name: 'contact_page')]
     public function contactAction(): Response
     {
         return $this->render('frontal/contact.html.twig');
     }
 
-    /**
-    * @Route("/profile", name="profile_page")
-     * games_page
-     */
+     #[Route('/profile', name: 'profile_page')]
     public function profileAction(): Response
     {
         return $this->render('frontal/perfil.html.twig');
