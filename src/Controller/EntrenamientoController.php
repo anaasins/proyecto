d53eed55782894283017c6fa1927754ef4acc88d
anaasins <asinsanna@gmail.com>
2022-03-28 11:02:37 +0200

    Avances parte entrenamiento

--- src/Controller/EntrenamientoController.php
+++ src/Controller/EntrenamientoController.php
@@ -6,13 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Entrenamiento;
+use App\Entity\Usuario;
+use App\Entity\Ejercicio;
 use App\Repository\EntrenamientoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class EntrenamientoController extends AbstractController
 {
   #[Route('/entrenar/{id}', name: 'app_entrenar')]
-  public function entrenarAction(int $id): Response
+  public function entrenarAction(int $id, EntityManagerInterface $entityManager): Response
   {
-      return $this->render('ejercicios_disponibles/'.$id.'/index.html.twig');
+      $user= $this->getUser();
+      $entrenamientoRepository = $entityManager -> getRepository(Entrenamiento::class);
+      $entrenamiento = $entrenamientoRepository->findOneBy(array('usuario'=>$user->getId(), 'ejercicio'=>$id), array('id'=>'DESC'));
+      $ejercicio = $entityManager -> getRepository(Ejercicio::class)->findOneBy(array('id'=>$id));
+      return $this->render('ejercicios_disponibles/'.$id.'/index.html.twig', array('entrenamiento' => $entrenamiento,
+                                                                                    'ejercicio' => $ejercicio
+                                                                                  ));
+  }
+
+  #[Route('/guardarEntrenamiento', name: 'app_guardarEntrenamiento')]
+  public function guardarEntrenamientoAction(): Response
+  {
+
   }
 }
